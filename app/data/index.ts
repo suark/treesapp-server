c8fa9eb68a31bcb839be0c400432ba9c6ecaa94a
suark <brian.kraus@farmersedge.ca>
2018-09-06 16:36:44 -0600

    more small changes

--- app/data/index.ts
+++ app/data/index.ts
@@ -1,24 +1,3 @@
-import { TreeMarkerMap } from "../types";
-
-class TreeCache {
-    private _lastUpdated: number
-    private _data: TreeMarkerMap
-    
-    constructor() {
-        this._lastUpdated = Date.now()
-        this._data = {}
-    }
-
-    get lastUpdated(): number {
-        return this._lastUpdated
-    } 
-    get data(): TreeMarkerMap {
-        return this._data
-    }
-    set data(newData: TreeMarkerMap) {
-        this._lastUpdated = Date.now()
-        this._data = newData
-    }
-}
+import { TreeCache } from '../classes'
 
 export const treeCache = new TreeCache()
